58a0e0d2266671323f1f77460b2e724b9c4e5c39
576924406 <35132860+576924406@users.noreply.github.com>
2018-01-05 16:37:52 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    "score"      => 2022,  // 你想要的分数
-    "session_id" => '', // 你的 session_id;
+    "score"      => 5201314,  // 你想要的分数
+    "session_id" => 'owu6QFZvSWcjBIW2MRViTRY7QZd7z52CMpyY1sTFQklTJgtaF9Ot5M7+rK79I82W3X8RcIK4bTTrd6k1EO4a6K7KsTZDuXkjmBKuwPXFiV0MDXUjmKWCjcmkRTz/zMRQvrqBOaELO4EH5AUZZamsvg\u003d\u003d', // 你的 session_id;
 ];
